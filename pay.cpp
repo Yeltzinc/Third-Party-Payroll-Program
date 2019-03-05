ae1ad1a250ec3f65c7f7f5bd72a8f94d991e5513
Yeltzinc <35970214+Yeltzinc@users.noreply.github.com>
2019-03-04 23:52:53 -0800

    Step 7

---
+++ pay.cpp
@@ -0,0 +1,62 @@
+//step 4
+#include "person.cpp"
+#include<stdio.h>
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<string>
+//step 5 including a vector
+#include<vector>
+using namespace std;
+//step 7 read data that read from input.txt and stores into employees vector.
+void readData(vector<Person>&employees)
+{
+    //varaibles from the employees using given code.
+    string firstName;
+    string lastName;
+    int employeeID;
+    string comapnyName;
+    float hoursWorked;
+    float payRate;
+    //opens file input
+    ifstream input
+    input.open("input.txt");
+    
+    //reading file and copying it into person for amount of employees inside of the input.txt file.
+    while(input>>firstName>>lastName>>employeeID>>companyName>>hoursWorked>>payRate)
+    {
+        //declaring p to save info to
+        Person p;
+        //setting the variables into p
+        p.setFirstName(firstName);
+        p.setLastName(lastName);
+        p.setEmployeeId(employeeID);
+        p.setCompanyName(companyName);
+        p.setHoursWorked(hoursWorked);
+        p.setPayRate(payRate);
+        //pushes back into vector
+        employees.push_back(p);
+    }
+    //saves vector size 
+    for(int i=0;i<employees.size();i++)
+    {
+        cout<<employees.at(i).fullName()<<endl;
+    }
+    //closes file
+    input.close();
+}
+
+
+
+int main()
+{
+//creating a vector for employees.
+    vector<Person> employees;
+//step 6 vector that holds companyNames
+    vector<string>companyNames;
+//step 7 read data that read from input.txt and stores into employees vector.
+    readData(employees);
+    return 0;
+    
+}
+
---
+++ person.cpp
@@ -0,0 +1,65 @@
+#include "person.hpp"
+Person::Person()
+{
+//default constructor
+}
+//setters
+void Person::setFirstName(string first)
+{
+    //using given variables
+    firstName=first;
+}
+void Person::setLastName(string last)
+{
+    lastName=last;
+}
+void Person::setEmployeeId(int id)
+{
+    employeeID=id;
+}
+void Person::setCompanyName(string coName)
+{
+    companyName=coName;
+}
+void Person::setPayRate(float prate)
+{
+    payRate=prate;
+}
+void Person::setHoursWorked(float hours)
+{
+    hoursWorked=hours;
+}
+//getters
+string Person::getFirstName()
+{
+    return firstName;
+}
+string Person::getLastName()
+{
+    return lastName;
+}
+int Person::getemployeeID()
+{
+    return employeeID;
+}
+string Person::getCompanyName()
+{
+    return companyName;
+}
+float Person::getPayRate()
+{
+    return payRate;
+}
+float Person::getHoursWorked()
+{
+    return hoursWorked;
+}
+//arithmitic functions
+float Person::totalPay()
+{
+    return hoursWorked*payRate;
+}
+string Person::fullName()
+{
+    return firstName+" "+lastName;
+}
